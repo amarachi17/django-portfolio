1dd63fa933b88a147fb1fb2db3266ce4d0136722
confidence <confidencenkeonye@gmail.com>
2025-10-21 13:27:27 +0100

    Fixed a bug

--- portfolio_project/portfolio/models.py
+++ portfolio_project/portfolio/models.py
@@ -7,7 +7,7 @@ class Project(models.Model):
     github_link = models.URLField(blank=True, null=True)
     live_demo = models.URLField(blank=True, null=True)
     image = models.ImageField(upload_to='projects/', blank=True, null=True)
-    create_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
