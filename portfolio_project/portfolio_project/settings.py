7e93a21b27ce9778cba13f34fc8ae2e5c5e94272
confidence <confidencenkeonye@gmail.com>
2025-10-31 14:35:04 +0100

    Added new changes to settings file

--- portfolio_project/portfolio_project/settings.py
+++ portfolio_project/portfolio_project/settings.py
@@ -134,7 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'portfolio_project' / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'portfolio' / 'static']
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
