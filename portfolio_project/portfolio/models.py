84cb8554130ac216c863c2d2ff42c5687f2360f7
confidence <confidencenkeonye@gmail.com>
2025-12-03 23:08:36 +0100

    Created project image

--- portfolio_project/portfolio/models.py
+++ portfolio_project/portfolio/models.py
@@ -11,5 +11,12 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+class ProjectImage(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to='project_detail')
+    caption = models.CharField(max_length=255, blank=True, null=True)
 
+    def __str__(self):
+        return f"Image for {self.project.title}"
+    
 
