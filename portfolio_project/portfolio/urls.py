48199381c2e2e62c8d3c3dec35355f9fd3930c1d
confidence <confidencenkeonye@gmail.com>
2025-12-03 23:09:11 +0100

    Fixed a bug

--- portfolio_project/portfolio/urls.py
+++ portfolio_project/portfolio/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path 
 from . import views
+from .views import project_detail
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
+    path('project/<int:pk>/', project_detail, name="project_detail"),
 ]
